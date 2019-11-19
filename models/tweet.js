9e25718ac1b64f53a70e8a8db5170d91f9fd6748
Harrison Ngan <hngan@Harrisons-MacBook-Pro.local>
2019-11-19 13:21:42 -0500

    fix? media?

--- models/tweet.js
+++ models/tweet.js
@@ -32,7 +32,6 @@ const TweetSchema = new Schema({
     },
     users:[{type: Schema.Types.ObjectId, ref:'User'}], //keep tracks of people who already liked
     parent: {type: Schema.Types.ObjectId, ref: 'Tweet'},
-    media: [{type: String}],
     replies:[{type: Schema.Types.ObjectId, ref: 'Tweet'}],
     interest:{type: Number, default: 0}
 });
