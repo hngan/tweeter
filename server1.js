ea4d304c9ef2e9f7b91e31ce9d99a5391c67b696
Harrison Ngan <hngan@Harrisons-MBP.fios-router.home>
2019-12-02 02:30:26 -0500

    tweaked loging

--- server1.js
+++ server1.js
@@ -50,7 +50,8 @@ var transporter = nodemailer.createTransport({
       if(resp.length === 0){
         db.User.find({email:req.body.email}).lean().then((data) =>{
          if(data.length === 0){
-          amqp.connect('amqp://localhost', function(error0, connection) {
+        db.User.create(req.body).then(data =>{
+            amqp.connect('amqp://localhost', function(error0, connection) {
             if (error0) {
                 throw error0;
             }
@@ -62,15 +63,16 @@ var transporter = nodemailer.createTransport({
                 channel.assertQueue(queue, {
                     durable: true
                 });
-                channel.sendToQueue(queue, Buffer.from(JSON.stringify(req.body)), {
+                channel.sendToQueue(queue, Buffer.from(req.body.email), {
                     persistent: true
                 });
             });
             setTimeout(function() { 
-              connection.close(); 
+              connection.close();}, 50);
               res.status(200).json({status:"OK"});
-              }, 50);
-        });           
+        });
+        })
+                     
           }
             else{
               res.status(500).json({status:"error"})
@@ -244,42 +246,39 @@ var transporter = nodemailer.createTransport({
 
 //MILESTONE 2 STUFF
 app.delete('/item/:id', (req, res)=>{
-  console.log("DOING A DELETE")
-  if(req.session.userId)
-  db.Tweet.find({_id: req.params.id}).lean().then((data)=>{
-    if(data.length === 0){
-      res.status(500).json({status:"error"});
-    }
-    else{
-    if(req.session.username !== data[0].username){
-      res.status(500).json({status:"error"});}
+    console.log("DOING A DELETE")
+    if(req.session.userId)
+    db.Tweet.find({_id: req.params.id}).lean().then((data)=>{
+      if(data.length === 0){
+        res.status(500).json({status:"error"});
+      }
       else{
-        amqp.connect('amqp://localhost', function(error0, connection) {
-          if (error0) {
-              throw error0;
+      if(req.session.username !== data[0].username){
+        res.status(500).json({status:"error"});}
+        else{
+          let query = "DELETE FROM tweeter WHERE id = ?";
+        if(data[0].media)
+        for(let i = 0; i < data[0].media.length; i++){
+            client.execute(query, [data[0].media[i]]).then((err)=>{
+              if(err)
+              console.log(err)
+            });
           }
-            connection.createChannel(function(error1, channel) {
-              if (error1) {
-                  throw error1;
-              }
-              var queue = 'delete_queue';
-              channel.assertQueue(queue, {
-                  durable: true
-              });
-              channel.sendToQueue(queue, Buffer.from(JSON.stringify(data[0])), {
-                  persistent: true
-              });
+          db.Tweet.deleteOne({_id:req.params.id}, (err)=>{
+            if(err){
+              res.status(500).json({status:"error"});
+            }
+            else{
+              db.User.findOneAndUpdate({username:req.session.username},{ $pull: {tweets: req.params.id} }).then((resp)=>{
+                res.status(200).json({status:"OK"});
+              })
+            }    
           });
-          setTimeout(function() { 
-            connection.close(); 
-            }, 50);
-      }); 
-      res.status(200).json({status:"OK"}); 
-    }}
-  }) 
-  else
-    res.status(500).json({status:"error"});
-});
+        }}
+    }) 
+    else
+      res.status(500).json({status:"error"});
+  });
 
 app.get('/user/:username', (req, res)=>{
   db.User.find({username:req.params.username}).then(data=>{
@@ -522,8 +521,6 @@ amqp.connect('amqp://localhost', function(error, connection) {
         channel.prefetch(1);
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
         channel.consume(queue, function(files) {
-          let newUser =JSON.parse(files.content.toString())
-          db.User.create(newUser).then((dbmodel)=>{
             const message = {
               from: 'hnganMailingService356@gmail.com',
               to:newUser.email,
@@ -534,38 +531,7 @@ amqp.connect('amqp://localhost', function(error, connection) {
             transporter.sendMail(message, function (err, info) {
               if(err)
                 console.log(err)
-               });   
-          })
-          .then(result => {
-            channel.ack(files);
-          });
-        }, {
-            noAck: false
-        });
-    });
-});
-
-amqp.connect('amqp://localhost', function(error, connection) {
-    connection.createChannel(function(error, channel) {
-        var queue = 'delete_queue';
-        channel.assertQueue(queue, {
-            durable: true
-        });
-        channel.prefetch(1);
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-        channel.consume(queue, function(files) {
-          let item =JSON.parse(files.content.toString())
-          let query = "DELETE FROM tweeter WHERE id = ?";
-      if(item.media)
-      for(let i = 0; i < item.media.length; i++){
-          client.execute(query, [item.media[i]]).then((err)=>{
-            if(err)
-            console.log(err)
-          });
-        }
-        db.Tweet.deleteOne({_id:item._id}, (err)=>{
-            db.User.findOneAndUpdate({username:req.session.username},{ $pull: {tweets: item._id} }).then((resp)=>{})  
-        })
+               })
           .then(result => {
             channel.ack(files);
           });
